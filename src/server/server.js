d1a29bfd6cbfca3023f3e0ee511871b7a5d4fcce
Jesus Martinez <amser@LAPTOP-PN0APQTA>
2024-11-01 18:34:46 -0600

    add import and export with es

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,11 @@
-const express=require('express')
-const path=require('path')
+//Forma antigua de importar
+/*const express=require('express')
+const path=require('path')*/
+import express from 'express'
+import path from 'path'
 
 
+/*Forma antigua de exportar
 const startServer=(options)=>{
     const{port,public_path='public'}=options
     console.log(port)
@@ -21,4 +25,22 @@ const startServer=(options)=>{
 
 module.exports={
     startServer
+}*/
+
+
+export const startServer=(options)=>{
+    const{port,public_path='public'}=options
+    console.log(port)
+    console.log(public_path)
+    const app=express();
+    //para usar midleware se usa USE, es propio de express
+    app.use(express.static(public_path))//contenido estatico que expone 
+    app.get('*',(req,res)=>{
+        const indexPath=path.join(__dirname + `../../../${public_path}/index.html`)
+        res.sendFile(indexPath);
+
+    })
+    app.listen(port,()=>{
+        console.log(`escuchando en el puerto ${port}`)
+    })
 }
